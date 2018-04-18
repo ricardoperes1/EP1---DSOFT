e9734bdb6a3ec3d5f0c184eda2002b8c8da4d252
VictorVazquez25 <38338488+VictorVazquez25@users.noreply.github.com>
2018-04-18 13:15:08 -0300

    Update and rename projeto_iniciado.py to projeto_inicio_feito.py

--- projeto_inicio_feito.py
+++ projeto_inicio_feito.py
@@ -1,3 +1,4 @@
+
 #mostra o que cada número é
 print("0: Sair")
 print("1: Adicionar Item")
@@ -17,18 +18,35 @@ produtos = {
         'pepino': {'Quantidade': 0},
         }
 
-
-
 if menu_inicial == 0:
     print("Até Mais")
     
 elif menu_inicial == 1:
-    novo_produto=input("Novo Produto: ")
+    novo_produto = input("Novo Produto: ")
     quantidade = int(input("Quantidade Inicial: "))
     while quantidade < 0:
-        print("Quantidade inválida")
+        print("Quantidade Inválida")
         quantidade = int(input("Quantidade Inicial: "))
     produtos[novo_produto]= {"Quantidade":quantidade}
     print("Item Adicionado")
+    
+elif menu_inicial == 2:
+    remove_produto = input("Retirar o Produto: ")
+    del produtos[remove_produto]
+    print("Item Removido")
+    
+elif menu_inicial == 3:
+    produto_alterado = input("Nome do Produto: ")
+    while produto_alterado not in produtos:
+        print("Produto Não Encontrado")
+        produto_alterado = input("Nome do Produto: ")
+    nova_quantidade = int(input("Nova Quantidade: "))
+    produtos[produto_alterado] = {"Quantidade":nova_quantidade}
 
+elif menu_inicial == 4:
+    for p, q in produtos.items():
+        print("{0}: {1}".format(p, q["Quantidade"]))
+    
+    
+    
 
