31ea0ae8c411254a40155a3511fa89e910f91e26
rafaelff3 <rafael.fonte.freire@gmail.com>
2018-04-21 23:13:37 -0300

    Json aplicado

--- parte1.py
+++ parte1.py
@@ -1,3 +1,8 @@
+import json
+
+with open('produtos.json','r') as dados:#abre o arquivo
+    produtos = json.load(dados)#lê o arquivo e salva na variável 'produtos'em dict
+
 
 #mostra o que cada número é
 print("0: Sair")
@@ -6,19 +11,12 @@ print("2: Remover Item")
 print("3: Alterar Item")
 print("4: Imprimir Estoque")
 
-print("No git! Vai Corinthians!!!")
-
 #escolha do usuario
 menu_inicial= int(input("Escolha o Numero: "))
 
 #checando 
 #print(menu_inicial) 
 #criando um estoque inicial dos produtos
-produtos = {
-        'batata': {'Quantidade': 0},
-        'cenoura': {'Quantidade': 0},
-        'pepino': {'Quantidade': 0},
-        }
 
 if menu_inicial == 0:
     print("Até Mais")
@@ -31,9 +29,12 @@ elif menu_inicial == 1:
         quantidade = int(input("Quantidade Inicial: "))
     produtos[novo_produto]= {"Quantidade":quantidade}
     print("Item Adicionado")
-    
+
 elif menu_inicial == 2:
     remove_produto = input("Retirar o Produto: ")
+    while remove_produto not in produtos:
+        print("Produto Não Encontrado")
+        remove_produto = input("Nome do Produto: ")    
     del produtos[remove_produto]
     print("Item Removido")
     
@@ -41,13 +42,19 @@ elif menu_inicial == 3:
     produto_alterado = input("Nome do Produto: ")
     while produto_alterado not in produtos:
         print("Produto Não Encontrado")
-        produto_alterado = input("Nome do Produto: ")
+        produto_alterado = input("Nome do Produto: ")    
+    print('Quantidade Registrada: {0}'.format(produtos[produto_alterado]['Quantidade']))
     nova_quantidade = int(input("Nova Quantidade: "))
     produtos[produto_alterado] = {"Quantidade":nova_quantidade}
+    print('Quantidade Alterada')
 
 elif menu_inicial == 4:
     for p, q in produtos.items():
         print("{0}: {1}".format(p, q["Quantidade"]))
-    
-    
-    
+
+
+#converte de volta em string, organiza o arquivo e lista em ordem alfabética
+with open('produtos.json','w') as dados:
+    produtos_alterados = json.dumps(produtos,indent=2,sort_keys=True)
+    dados.write(produtos_alterados)
+
